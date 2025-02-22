a0ceeb9a8017239d764e534c9338c1d1b056e2a4
Richard Lambert <hello@richardlambert.dev>
2025-02-22 13:16:42 +0000

    Setup tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
 const App = () => {
-  return <h1>AIS Clinent</h1>;
+  const customColours = [
+    { background: 'white', text: 'grey-500' },
+    { background: 'orange-500', text: 'white' },
+    { background: 'blue-500', text: 'white' },
+    { background: 'grey-300', text: 'grey-500' },
+    { background: 'grey-400', text: 'grey-500' },
+    { background: 'grey-500', text: 'white' },
+  ];
+
+  return (
+    <div className="h-screen flex gap-4 flex-col items-center justify-center">
+      <h1 className="text-2xl">AIS Tracker</h1>
+      <section>
+        <div className="flex gap-2 items-center">
+          <h2 className="text-xl me-4">Colours</h2>
+          {customColours.map(({ background, text }) => (
+            <span
+              key={background}
+              className={`w-24 h-20 flex items-center justify-center shadow-xl rounded-xl bg-${background} text-${text}`}
+            >
+              {background}
+            </span>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default App;
