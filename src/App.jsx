69eae1c7150cce14d0033bc14a77981edf27e755
Richard Lambert <hello@richardlambert.dev>
2025-02-22 14:35:41 +0000

    Fix tailwindcss interpolated classnames

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ const App = () => {
   const { state, dispatch } = useAppContext();
 
   const customColours = [
-    { background: 'white', text: 'grey-500' },
-    { background: 'orange-500', text: 'white' },
-    { background: 'blue-500', text: 'white' },
-    { background: 'grey-300', text: 'grey-500' },
-    { background: 'grey-400', text: 'grey-500' },
-    { background: 'grey-500', text: 'white' },
+    { colour: 'white', background: 'bg-white', text: 'text-grey-500' },
+    { colour: 'orange-500', background: 'bg-orange-500', text: 'text-white' },
+    { colour: 'blue-500', background: 'bg-blue-500', text: 'text-white' },
+    { colour: 'grey-300', background: 'bg-grey-300', text: 'text-grey-500' },
+    { colour: 'grey-400', background: 'bg-grey-400', text: 'text-grey-500' },
+    { colour: 'grey-500', background: 'bg-grey-500', text: 'text-white' },
   ];
 
   return (
@@ -17,12 +17,12 @@ const App = () => {
       <h1 className="text-2xl">AIS Tracker</h1>
       <div className="flex gap-2 items-center">
         <h2 className="text-xl me-4">Colours</h2>
-        {customColours.map(({ background, text }) => (
+        {customColours.map(({ colour, background, text }) => (
           <span
-            key={background}
-            className={`w-24 h-20 flex items-center justify-center shadow-xl rounded-xl bg-${background} text-${text}`}
+            key={colour}
+            className={`w-24 h-20 flex items-center justify-center shadow-xl rounded-xl ${background} ${text}`}
           >
-            {background}
+            {colour}
           </span>
         ))}
       </div>
